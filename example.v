5d4a3cb1bd878202cb76fb6bc4514b343c5cd551
Julia Desmazes <julia.desmazes@gmail.com>
2023-11-24 17:11:26 -0600

    7SEG: Testing out 7 seg display with counter

--- example.v
+++ example.v
@@ -1,3 +1,4 @@
+/*
 module mul_X#(
 	parameter W = 32,
 	parameter [W-1:0] X = 5
@@ -7,22 +8,50 @@ module mul_X#(
 );
 	assign d_o = d_i * X;
 endmodule
+*/
 
-module top (input a, b,c, output y,l);
-	localparam W = 32;
-  	wire [W-1:0] x;
-  	wire [W-1:0] r0;
-	
-	assign x = { a, 29'b0,b, a};
 
-	mul_X #(.W(W),.X(6))m_5(
-		.d_i(x),
-		.d_o(r0)
-	);	
+module top (
+	input  clk, 
+	input  switch0_i,
+	input  switch1_i, 
+	output led0_o,
+	output led1_o,
+	output led2_o,
+	output led3_o,
+	output [6:0] segA_o
+	);
+	wire reset; 
+	assign reset = switch0_i;
 
-	assign y = |(r0);
-	/* led */
-	assign l = c;
+	/* counter */
+	reg   [27:0] cnt_q; 
+	always @(posedge clk or posedge reset)begin
+		if(reset)begin
+			cnt_q <= 'b0;
+		end else begin
+			cnt_q <= cnt_q + 'd1;
+		end
+	end
+	asc_to_7seg m_7seg(
+		.bin({4'b0, cnt_q[27:24]}),
+		.seg(segA_o)
+	);
 
+	/* no sync */
+	assign led0_o = switch0_i;
+
+	/* 2ff sync */
+	reg meta_q;
+	reg stable_q;
+	always @(posedge clk) begin
+		meta_q <= switch0_i;
+		stable_q <= meta_q;
+	end	
+	assign led1_o = stable_q;
+
+	/* default led */
+	assign led2_o = 1'b0;
+	assign led3_o = 1'b0;
 endmodule
 
--- makefile
+++ makefile
@@ -1,13 +1,17 @@
 PROJ=example
 
+# board support package
+BSP=bsp.pcf
 # board using package vq100 
 PKG=vq100
 
-$(PROJ).blif: $(PROJ).v $(PROJ).pcf
-	yosys -p 'synth_ice40 -top top -blif example.blif' -p 'read -sv $<' $< 
+sv_files=example.v asc_to_7seg.v
 
-$(PROJ).asc: $(PROJ).blif
-	arachne-pnr -P $(PKG) -d 1k -o example.asc -p example.pcf example.blif
+$(PROJ).blif: $(sv_files)
+	yosys -p 'synth_ice40 -top top -blif example.blif' -p 'read -sv $^' $^ 
+
+$(PROJ).asc: $(PROJ).blif $(BSP)
+	arachne-pnr -P $(PKG) -d 1k -o example.asc -p $(BSP) example.blif
 	
 $(PROJ).bin: $(PROJ).asc
 	icepack example.asc example.bin
